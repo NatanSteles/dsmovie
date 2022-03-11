e50df893c108179224e182fad4da028c595cd21f
Natan Teles <natansteles@gmail.com>
2022-03-11 10:49:22 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+function Form () {
+
+    return (
+        <h1>Páginade formulário</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,8 @@
+function Listing () {
+
+    return (
+        <h1>Páginade lsitagem</h1>
+    );
+}
+
+export default Listing;
